3958be5465a544fd7ab0ec9a587a0ea6c6de5e09
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2024-02-07 22:49:22 +0630

    Add DataModel

---
+++ SDH_BlazorTraining/Models/BlogDataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SDH_BlazorTraining.Models
+{
+    public class BlogDataModel
+    {
+        public int Blog_Id { get; set; }
+        public String Blog_Title { get; set; }
+        public String Blog_Author { get; set; }
+        public String Blog_Content { get; set; }
+
+    }
+}

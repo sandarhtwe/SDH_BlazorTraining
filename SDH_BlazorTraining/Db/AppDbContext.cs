50a7139d52f3a6bef8d3ca852019eb6c8cd32cd0
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2024-02-07 22:49:00 +0630

    Add Db Context

---
+++ SDH_BlazorTraining/Db/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SDH_BlazorTraining.Models;
+
+namespace SDH_BlazorTraining.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}

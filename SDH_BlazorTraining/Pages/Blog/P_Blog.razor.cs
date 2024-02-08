1436fd913845c743e4996e04bff2a237fc0d286f
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2024-02-08 17:05:02 +0630

    -

--- SDH_BlazorTraining/Pages/Blog/P_Blog.razor.cs
+++ SDH_BlazorTraining/Pages/Blog/P_Blog.razor.cs
@@ -2,5 +2,23 @@
 {
     public partial class P_Blog
     {
+        private BlogDataModel reqModel { get; set; } = new BlogDataModel();
+
+        private void Cancel()
+        {
+           reqModel = new BlogDataModel();
+        }
+
+        private async Task Save()
+        {
+            Console.WriteLine(reqModel.Blog_Title);
+            Console.WriteLine(reqModel.Blog_Author);
+            Console.WriteLine(reqModel.Blog_Content);
+
+            await _dbContext.Blogs.AddAsync(reqModel);
+            await _dbContext.SaveChangesAsync();
+
+            reqModel=new BlogDataModel();
+        }
     }
 }

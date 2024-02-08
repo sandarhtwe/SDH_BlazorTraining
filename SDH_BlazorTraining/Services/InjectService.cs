0f974f5c88637c7ee5262a07c1fc772298f8781e
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2024-02-08 17:05:21 +0630

    [Add] Services

---
+++ SDH_BlazorTraining/Services/InjectService.cs
@@ -0,0 +1,25 @@
+﻿using MudBlazor;
+
+namespace SDH_BlazorTraining.Services
+{
+    public class InjectService
+    {
+        private readonly IDialogService _dialogService;
+
+        public InjectService(IDialogService dialogService)
+        {
+            _dialogService = dialogService;
+        }
+
+        public void ShowMessage(ResponseModel response)
+        {
+            var parameters = new DialogParameters<MessageBox>();
+            parameters.Add(x => x.Response, response);
+
+            var options = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall };
+
+            _dialogService.Show<MessageBox>(response.RespType.ToString(), parameters, options);
+        }
+    }
+    }
+}
